525cb557a9d1fa9ac79e509d9dab6ea1031b3c96
Praneet Kumar Singh <praneetsingh242@gmail.com>
2023-01-22 22:51:57 +0530

    First Table Layout Exercise

---
+++ app/src/main/java/com/example/firstrelativeapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.firstrelativeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    //For formatting code --> Ctrl + shift + alt + l
+    }
+}
